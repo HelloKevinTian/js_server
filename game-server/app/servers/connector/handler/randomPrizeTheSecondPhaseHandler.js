bc3eec28ae082784d5629e74f2175d2ebefcddaa
tianwen <tianwen@chukong-inc.com>
2015-04-07 16:17:48 +0800

    new notice

--- game-server/app/servers/connector/handler/randomPrizeTheSecondPhaseHandler.js
+++ game-server/app/servers/connector/handler/randomPrizeTheSecondPhaseHandler.js
@@ -4,6 +4,7 @@
 var handlerMgr = require("./../handlerMgr");
 var consts = require("../../../util/consts");
 var pomelo = require('pomelo');
+var specialPrizeFlag = require("../../../nosql/redis_special_prize_flag")
 
 handlerMgr.handler(consts.TYPE_MSG.TYPE_RANDOM_PRIZE_THE_SECOND_PHASE, function(msg, session, next) {
     var channel = msg.channel;
@@ -20,6 +21,7 @@ handlerMgr.handler(consts.TYPE_MSG.TYPE_RANDOM_PRIZE_THE_SECOND_PHASE, function(
         count = 12;
     }
     var free_flag = 1;
+    var count_index = null;
     var activity = {};
     var activity_wrapper = pomelo.app.get('activity_wrapper');
     activity_wrapper.get(channel,version,function(activity_json) {
@@ -38,32 +40,65 @@ handlerMgr.handler(consts.TYPE_MSG.TYPE_RANDOM_PRIZE_THE_SECOND_PHASE, function(
             if(1 == free_flag && single_gacha == "true" ){
                 free_flag = 0;
             }
-            for(var i = 0; i < count; ++i){
-                var gacha_array = new Array();
-                for(var j = 0; j < activity.gacha_random_num; ++j){
-                    var prize = random_prize_the_second_phase_wrapper.random();
-                    if(!prize){
-                        continue;
-                    }
-                    gacha_array.push(prize);
+            specialPrizeFlag.get(device_guid,function(replyData){
+                if(null != replyData){
+                    count_index = JSON.parse(replyData)
                 }
-                for(var j = 0; j < activity.gacha2_random_num; ++j){
-                    var prize = random_prize_the_second_phase_wrapper.random2();
-                    if(!prize){
-                        continue;
+
+                for(var i = 0; i < count; ++i){
+                    var gacha_array = new Array();
+                    var tempValue = 0;
+
+                    if(null === count_index){
+                        //first time
+                        count_index = 2
+                        var prize = random_prize_the_second_phase_wrapper.random3();
+                        if(!prize){
+                            continue;
+                        }
+                        gacha_array.push(prize);
+                    } else if(count_index === 3){
+                        //third time
+                        count_index++;
+                        var prize = random_prize_the_second_phase_wrapper.random3();
+                        if(!prize){
+                            continue;
+                        }
+                        gacha_array.push(prize);
+                        gacha_array.push(prize);
+                        tempValue = -1;
+                    } else {
+                        if(count_index === 2){
+                            count_index++;
+                        }
+                        for(var j = 0; j < activity.gacha_random_num; ++j){
+                            var prize = random_prize_the_second_phase_wrapper.random();
+                            if(!prize){
+                                continue;
+                            }
+                            gacha_array.push(prize);
+                        }
                     }
-                    gacha_array.push(prize);
+
+                    for(var j = 0; j < (activity.gacha2_random_num + tempValue); ++j){
+                        var prize = random_prize_the_second_phase_wrapper.random2();
+                        if(!prize){
+                            continue;
+                        }
+                        gacha_array.push(prize);
+                    }
+                    gacha_result.push(gacha_array);
                 }
-                gacha_result.push(gacha_array);
-            }
-            random_prize_the_second_phase_wrapper.set(device_guid,free_flag);
-            next(null, {
-                code: 0,
-                msg_id : msg.msg_id,
-                flowid : msg.flowid,
-                time:Math.floor(Date.now()/1000),
-                gacha_result : gacha_result,
-                use_ticket : msg.use_ticket
+                random_prize_the_second_phase_wrapper.set(device_guid,free_flag);
+                specialPrizeFlag.set(device_guid,count_index);
+                next(null, {
+                    code: 0,
+                    msg_id : msg.msg_id,
+                    flowid : msg.flowid,
+                    time:Math.floor(Date.now()/1000),
+                    gacha_result : gacha_result,
+                    use_ticket : msg.use_ticket == "true" ? "1": "0"
+                });
             });
         });
 
