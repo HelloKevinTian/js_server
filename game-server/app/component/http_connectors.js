59ee996db043138f1787631b96987a5ab3ee936d
tianwen <tianwen@chukong-inc.com>
2015-04-10 16:43:45 +0800

    format

--- game-server/app/component/http_connectors.js
+++ game-server/app/component/http_connectors.js
@@ -2,7 +2,7 @@
  * Created by King Lee on 2014/8/13.
  */
 var http = require("http");
-var qs = require( 'querystring' );
+var qs = require('querystring');
 var handlerMgr = require('../servers/connector/handlerMgr');
 var session = require('../util/session');
 var pomelo = require('pomelo');
@@ -21,7 +21,7 @@ var DEFAULT_PORT = 20000;
 var http_connectors = function(app, opts) {
     this.app = app;
     this.server = null;
-    this.port = opts.port ;
+    this.port = opts.port;
     this.host = opts.host;
     this.cluster = opts.cluster;
     //  a temp variable
@@ -32,7 +32,7 @@ http_connectors.name = '__http_connectors__';
 
 http_connectors.prototype.start = function(cb) {
     var cpu_num = require('os').cpus().length;
-    if(this.cluster){
+    if (this.cluster) {
         if (cluster.isMaster) {
             // Fork workers.
             for (var i = 0; i < cpu_num; i++) {
@@ -42,45 +42,47 @@ http_connectors.prototype.start = function(cb) {
             cluster.on('exit', function(worker, code, signal) {
                 http_logger.debug('worker ' + worker.process.pid + ' died');
             });
-        }
-        else{
+        } else {
             this.startHttpServer(cb);
         }
-    }else{
+    } else {
         this.startHttpServer(cb);
     }
 };
 
-http_connectors.prototype.startHttpServer = function(cb){
+http_connectors.prototype.startHttpServer = function(cb) {
     var self = this;
     this.server = http.createServer(function(req, res) {
         var url = req.url;
         var client_ip = req.connection.remoteAddress;
         http_logger.debug("new client coming ip:" + client_ip + " method:" + req.method + " url:" + url);
-        switch(req.method){
-            case 'GET':{
-                var args = self.parseGet(req, res);
-                args && self.dispatchMessage(args[1], args[0], req, res);
-                break;
-            }
-            case 'POST':{
-                self.parsePost(req,res,function(data){
-                    self.dispatchMessage(data,url,req,res);
-                });
-                break;
-            }
-            default:{
-                res.end();
-                break;
-            }
+        switch (req.method) {
+            case 'GET':
+                {
+                    var args = self.parseGet(req, res);
+                    args && self.dispatchMessage(args[1], args[0], req, res);
+                    break;
+                }
+            case 'POST':
+                {
+                    self.parsePost(req, res, function(data) {
+                        self.dispatchMessage(data, url, req, res);
+                    });
+                    break;
+                }
+            default:
+                {
+                    res.end();
+                    break;
+                }
         }
     });
-    this.server.listen( this.port );
+    this.server.listen(this.port);
     http_logger.debug("server listen at " + this.port + " as a component!");
     process.nextTick(cb);
 };
 
-http_connectors.prototype.afterStart = function (cb) {
+http_connectors.prototype.afterStart = function(cb) {
     console.log('http connectors after start');
     process.nextTick(cb);
 };
@@ -96,11 +98,11 @@ http_connectors.prototype.stop = function(force, cb) {
  * @param res
  * @returns {*}
  */
-http_connectors.prototype.parseGet = function(req, res){
+http_connectors.prototype.parseGet = function(req, res) {
     var str = req.url;
     if (str.indexOf('?') > -1) {
         var arr = String.prototype.split.call(req.url, '?');
-        return [arr[0],qs.parse(arr[1])];
+        return [arr[0], qs.parse(arr[1])];
     } else {
         return [str, null];
     }
@@ -112,7 +114,7 @@ http_connectors.prototype.parseGet = function(req, res){
  * @param res
  * @param cb
  */
-http_connectors.prototype.parsePost = function(req,res,cb){
+http_connectors.prototype.parsePost = function(req, res, cb) {
     var chunks = [];
     req.on('data', function(chunk) {
         chunks.push(chunk);
@@ -124,39 +126,38 @@ http_connectors.prototype.parsePost = function(req,res,cb){
         //  convert url format to normal!!
         cb(qs.parse(data));
     });
-    req.on('error',function(err){
+    req.on('error', function(err) {
         http_logger.debug('problem with request: ' + err.message);
     });
 };
 
-http_connectors.prototype.dispatchMessage = function(data,url,req,res){
-    if(url == "/test")
-    {
+http_connectors.prototype.dispatchMessage = function(data, url, req, res) {
+    if (url == "/test") {
         //  date for test
         data = qs.parse('msg={"context": "context", "msg_id": 2}&account=king_lee');
-    }
-    else if(url == "/status")
-    {
-        var result = {code :200,status:"ok"}
+    } else if (url == "/status") {
+        var result = {
+            code: 200,
+            status: "ok"
+        }
         res.end(JSON.stringify(result) + '\n', 'utf8');
         return;
-    }else if(url == "/favicon.ico")
-    {
+    } else if (url == "/favicon.ico") {
         //  do nothing
         return;
     }
     var msg = JSON.parse(data.msg);
     //  version mapping
-    if(msg.version){
-        if("1.2.9" == msg.version){
+    if (msg.version) {
+        if ("1.2.9" == msg.version) {
             msg.version = "1.2.8";
-        }else if("1.0.0" == msg.version || "0.9.0" == msg.version){
+        } else if ("1.0.0" == msg.version || "0.9.0" == msg.version) {
             //  version mapping for wandoujia
             msg.version = "2.2.0";
-        }else if("1.0.1" == msg.version ){
+        } else if ("1.0.1" == msg.version) {
             //  version mapping for wandoujia
             msg.version = "2.2.1";
-        }else if("1.0.2" == msg.version ){
+        } else if ("1.0.2" == msg.version) {
             //  version mapping for wandoujia
             msg.version = "2.2.2";
         }
@@ -167,19 +168,18 @@ http_connectors.prototype.dispatchMessage = function(data,url,req,res){
     statistics_wrapper.requestsPerHourInc();
     statistics_wrapper.requestsPerMinuteInc();
     statistics_wrapper.statistics_device(msg.deviceid);
-    http_logger.debug("before dispatchMessage ... %j", msg);
-    handlerMgr.trigger(msg.msg_id,msg,this.session,function(error,res_msg){
-        http_logger.debug("after dispatchMessage ... %j", res_msg);
-        if(0){
+    http_logger.debug("C2S: %j", msg);
+    handlerMgr.trigger(msg.msg_id, msg, this.session, function(error, res_msg) {
+        http_logger.debug("S2C: %j", res_msg);
+        if (0) {
             //  by default the encoding is 'utf8'.
             res.write(JSON.stringify(res_msg));
             res.end();
-        }
-        else{
+        } else {
             //  res.end:Finishes sending the request. If any parts of the body are unsent, it will flush them to the stream.
             //  If the request is chunked, this will send the terminating '0\r\n\r\n'.
             //  If data is specified, it is equivalent to calling request.write(data, encoding) followed by request.end().
-            res.end( JSON.stringify(res_msg));
+            res.end(JSON.stringify(res_msg));
         }
     });
 };
--- game-server/app/random_prize/random_prize_the_second_phase_wrapper.js
+++ game-server/app/random_prize/random_prize_the_second_phase_wrapper.js
@@ -16,129 +16,141 @@ var random_prize_the_second_phase_wrapper = function() {
     this.wight_array3 = [];
     this.wight_array4 = [];
     this.init();
-    if(0){
+    if (0) {
         this.test();
     }
 };
 
-random_prize_the_second_phase_wrapper.prototype.init = function(){
-    for(var i = 0; i < gacha_the_second_phase_json.length; ++i){
+random_prize_the_second_phase_wrapper.prototype.init = function() {
+    for (var i = 0; i < gacha_the_second_phase_json.length; ++i) {
         var wight_total_backup = this.wight_total;
         this.wight_total += gacha_the_second_phase_json[i].rate;
-        this.wight_array.push({"id":gacha_the_second_phase_json[i].id,"range":[wight_total_backup,this.wight_total]});
+        this.wight_array.push({
+            "id": gacha_the_second_phase_json[i].id,
+            "range": [wight_total_backup, this.wight_total]
+        });
     }
-    for(var i = 0; i < gacha_the_second_phase_json_2.length; ++i){
+    for (var i = 0; i < gacha_the_second_phase_json_2.length; ++i) {
         var wight_total_backup2 = this.wight_total2;
         this.wight_total2 += gacha_the_second_phase_json_2[i].rate;
-        this.wight_array2.push({"id":gacha_the_second_phase_json_2[i].id,"range":[wight_total_backup2,this.wight_total2]});
+        this.wight_array2.push({
+            "id": gacha_the_second_phase_json_2[i].id,
+            "range": [wight_total_backup2, this.wight_total2]
+        });
     }
-    for(var i = 0; i < gacha_the_second_phase_json_3.length; ++i){
+    for (var i = 0; i < gacha_the_second_phase_json_3.length; ++i) {
         var wight_total_backup3 = this.wight_total3;
         this.wight_total3 += gacha_the_second_phase_json_3[i].rate;
-        this.wight_array3.push({"id":gacha_the_second_phase_json_3[i].id,"range":[wight_total_backup3,this.wight_total3]});
+        this.wight_array3.push({
+            "id": gacha_the_second_phase_json_3[i].id,
+            "range": [wight_total_backup3, this.wight_total3]
+        });
     }
-    for(var i = 0; i < gacha_the_second_phase_json_4.length; ++i){
+    for (var i = 0; i < gacha_the_second_phase_json_4.length; ++i) {
         var wight_total_backup4 = this.wight_total4;
         this.wight_total4 += gacha_the_second_phase_json_4[i].rate;
-        this.wight_array4.push({"id":gacha_the_second_phase_json_4[i].id,"range":[wight_total_backup4,this.wight_total4]});
+        this.wight_array4.push({
+            "id": gacha_the_second_phase_json_4[i].id,
+            "range": [wight_total_backup4, this.wight_total4]
+        });
     }
 }
 
-random_prize_the_second_phase_wrapper.prototype.random = function(){
-    var random_value = Math.floor(Math.random()*this.wight_total);
+random_prize_the_second_phase_wrapper.prototype.random = function() {
+    var random_value = Math.floor(Math.random() * this.wight_total);
     var index = 0;
-    for(var i = 0; i < this.wight_array.length; ++i){
-        if(random_value >= this.wight_array[i].range[0] && random_value < this.wight_array[i].range[1]){
+    for (var i = 0; i < this.wight_array.length; ++i) {
+        if (random_value >= this.wight_array[i].range[0] && random_value < this.wight_array[i].range[1]) {
             index = i;
             break;
         }
     }
-    for(var j = 0; j < gacha_the_second_phase_json.length; ++j){
-        if(j == index){
+    for (var j = 0; j < gacha_the_second_phase_json.length; ++j) {
+        if (j == index) {
             return gacha_the_second_phase_json[j];
         }
     }
     return null;
 };
 
-random_prize_the_second_phase_wrapper.prototype.random2 = function(){
-    var random_value = Math.floor(Math.random()*this.wight_total2);
+random_prize_the_second_phase_wrapper.prototype.random2 = function() {
+    var random_value = Math.floor(Math.random() * this.wight_total2);
     var index = 0;
-    for(var i = 0; i < this.wight_array2.length; ++i){
-        if(random_value >= this.wight_array2[i].range[0] && random_value < this.wight_array2[i].range[1]){
+    for (var i = 0; i < this.wight_array2.length; ++i) {
+        if (random_value >= this.wight_array2[i].range[0] && random_value < this.wight_array2[i].range[1]) {
             index = i;
             break;
         }
     }
-    for(var j = 0; j < gacha_the_second_phase_json_2.length; ++j){
-        if(j == index){
+    for (var j = 0; j < gacha_the_second_phase_json_2.length; ++j) {
+        if (j == index) {
             return gacha_the_second_phase_json_2[j];
         }
     }
     return null;
 };
 
-random_prize_the_second_phase_wrapper.prototype.random3 = function(){
-    var random_value = Math.floor(Math.random()*this.wight_total3);
+random_prize_the_second_phase_wrapper.prototype.random3 = function() {
+    var random_value = Math.floor(Math.random() * this.wight_total3);
     var index = 0;
-    for(var i = 0; i < this.wight_array3.length; ++i){
-        if(random_value >= this.wight_array3[i].range[0] && random_value < this.wight_array3[i].range[1]){
+    for (var i = 0; i < this.wight_array3.length; ++i) {
+        if (random_value >= this.wight_array3[i].range[0] && random_value < this.wight_array3[i].range[1]) {
             index = i;
             break;
         }
     }
-    for(var j = 0; j < gacha_the_second_phase_json_3.length; ++j){
-        if(j == index){
+    for (var j = 0; j < gacha_the_second_phase_json_3.length; ++j) {
+        if (j == index) {
             return gacha_the_second_phase_json_3[j];
         }
     }
     return null;
 };
 
-random_prize_the_second_phase_wrapper.prototype.random4 = function(){
-    var random_value = Math.floor(Math.random()*this.wight_total4);
+random_prize_the_second_phase_wrapper.prototype.random4 = function() {
+    var random_value = Math.floor(Math.random() * this.wight_total4);
     var index = 0;
-    for(var i = 0; i < this.wight_array4.length; ++i){
-        if(random_value >= this.wight_array4[i].range[0] && random_value < this.wight_array4[i].range[1]){
+    for (var i = 0; i < this.wight_array4.length; ++i) {
+        if (random_value >= this.wight_array4[i].range[0] && random_value < this.wight_array4[i].range[1]) {
             index = i;
             break;
         }
     }
-    for(var j = 0; j < gacha_the_second_phase_json_4.length; ++j){
-        if(j == index){
+    for (var j = 0; j < gacha_the_second_phase_json_4.length; ++j) {
+        if (j == index) {
             return gacha_the_second_phase_json_4[j];
         }
     }
     return null;
 };
 
-random_prize_the_second_phase_wrapper.prototype.set = function(device_guid,current_card,free_flag){
-    redis_random_prize_the_second_phase_wrapper.set(device_guid,current_card,free_flag);
+random_prize_the_second_phase_wrapper.prototype.set = function(device_guid, current_card, free_flag) {
+    redis_random_prize_the_second_phase_wrapper.set(device_guid, current_card, free_flag);
 };
 
-random_prize_the_second_phase_wrapper.prototype.get = function(device_guid,cb){
-    redis_random_prize_the_second_phase_wrapper.get(device_guid,cb);
+random_prize_the_second_phase_wrapper.prototype.get = function(device_guid, cb) {
+    redis_random_prize_the_second_phase_wrapper.get(device_guid, cb);
 };
 
-random_prize_the_second_phase_wrapper.prototype.statistics_for_participant = function(device_guid){
+random_prize_the_second_phase_wrapper.prototype.statistics_for_participant = function(device_guid) {
     redis_random_prize_the_second_phase_wrapper.statistics_for_participant(device_guid);
 };
 
-random_prize_the_second_phase_wrapper.prototype.test = function(){
+random_prize_the_second_phase_wrapper.prototype.test = function() {
     var prize_statistics = {};
     var max_loop = 10000000;
-    for(var i = 0; i < max_loop; ++i){
+    for (var i = 0; i < max_loop; ++i) {
         var prize = this.random();
-        if(prize_statistics[prize.id]){
+        if (prize_statistics[prize.id]) {
             ++prize_statistics[prize.id];
-        }else{
+        } else {
             prize_statistics[prize.id] = 1;
         }
     }
-    require("fs").writeFile('prize.json', JSON.stringify(prize_statistics), 'utf8', function(err){
-        if(err){
+    require("fs").writeFile('prize.json', JSON.stringify(prize_statistics), 'utf8', function(err) {
+        if (err) {
             console.log('failed');
-        }else{
+        } else {
             console.log('ok');
         }
     });
